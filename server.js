194d0e079813b15ff011bf6cdd2c7a34a664b20e
Suzi Curran <suzicurran@gmail.com>
2019-05-26 21:42:30 -0400

    add start script, rename app to server

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const apiroutes = require('./routes/api_route.js');
 const { MONGO_ADMIN_PW } = require('./secrets');
-const app = express();
+const server = express();
 
-app.use(express.json()) //sets content-type to json
-app.use('/', apiroutes);
+server.use(express.json()) //sets content-type to json
+server.use('/', apiroutes);
 
 const connectionString = `mongodb+srv://admin:${MONGO_ADMIN_PW}@happy-packed-lunches-eg8oh.mongodb.net/test?retryWrites=true`;
 mongoose.connect(connectionString,{useNewUrlParser: true})
   .then(()=>{
-    app.listen(3000);
+    server.listen(3000);
     console.log('database connected!');})
   .catch(err => console.log(err));

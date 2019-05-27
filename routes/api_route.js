ef99df32266e07cd9920c0f3b27c82d7a9c4e220
Suzi Curran <suzicurran@gmail.com>
2019-05-27 19:39:53 -0400

    messy passport login implementation

--- routes/api_route.js
+++ routes/api_route.js
@@ -1,19 +1,34 @@
-const express = require('express');
-const userController = require('../controller/user_controller');
+const express = require("express");
+const userController = require("../controller/user_controller");
 const router = express.Router();
-const path = require('path');
+const passport = require("passport");
+const path = require("path");
 
-router.get('/', userController.showIndex);
+router.get(
+  "/",
+  userController.showIndex
+);
 
-const testAuthFile = path.resolve('./test-auth.html');
-router.get('/login', (req, res) => res.sendFile(testAuthFile));
-router.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
-router.get('/error', (req, res) => res.send("error logging in"));
+// temporary login for test purposes
+const testAuthFile = path.resolve("./test-auth.html");
+router.get("/login", (req, res) => res.sendFile(testAuthFile));
+router.get("/success", (req, res) =>
+  res.send("Welcome " + req.user.username + "!!")
+);
+router.get("/error", (req, res) => res.send("error logging in"));
+
+router.post(
+  "/",
+  passport.authenticate("local", { failureRedirect: "/error" }),
+  function(req, res) {
+    res.redirect("/success?username=" + req.user.id);
+  }
+);
 
 // Streak (soon: User?)
-router.get('/streak', userController.streak);
-router.post('/create', userController.create);
-router.patch('/increment', userController.increment);
-router.patch('/reset', userController.reset);
+router.get("/streak", userController.streak);
+router.post("/create", userController.create);
+router.patch("/increment", userController.increment);
+router.patch("/reset", userController.reset);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,64 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const apiroutes = require('./routes/api_route.js');
-const passport = require('passport');
+const express = require("express");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const apiroutes = require("./routes/api_route.js");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./model/user_model.js");
 const server = express();
 
+server.use(express.json()); //sets content-type to json
+server.use(session({
+  secret: 'happylunches'
+}));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(passport.initialize());
 server.use(passport.session());
-server.use(express.json()) //sets content-type to json
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use('/', apiroutes);
+server.use("/", apiroutes);
 
-const connectionString = `mongodb+srv://admin:${process.env.MONGO_ADMIN_PW}@happy-packed-lunches-eg8oh.mongodb.net/test?retryWrites=true`;
-mongoose.connect(connectionString,{useNewUrlParser: true})
-  .then(()=>{
+const connectionString = `mongodb+srv://admin:${
+  //process.env.MONGO_ADMIN_PW
+  "tochiginoichigo"
+}@happy-packed-lunches-eg8oh.mongodb.net/test?retryWrites=true`;
+mongoose
+  .connect(connectionString, { useNewUrlParser: true })
+  .then(() => {
     server.listen(process.env.PORT);
-    console.log('database connected!');})
+    console.log("database connected!");
+  })
   .catch(err => console.log(err));
 
-  passport.serializeUser(function(user, cb) {
-    cb(null, user.id);
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne(
+      {
+        username: username
+      },
+      function(err, user) {
+        if (err) {
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false);
+        }
+
+        if (user.password != password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      }
+    );
+  })
+);
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findById(id, function(err, user) {
+    cb(err, user);
   });
-  
-  passport.deserializeUser(function(id, cb) {
-    User.findById(id, function(err, user) {
-      cb(err, user);
-    });
-  });
+});
